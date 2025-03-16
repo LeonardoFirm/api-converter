2134acfd14ecd10c08e7c5914794c2a4d30b0ff9
Leonardo Firme <leonardo.ads.ti@gmail.com>
2025-03-15 23:46:46 -0300

    Resolvendo conflitos

--- src/server.js
+++ src/server.js
@@ -4,13 +4,8 @@ import { PORT, OUTPUT_DIR, UPLOAD_DIR } from './config.js';
 import router from './routes.js';
 import fs from 'fs';
 import swaggerSetup from './swagger.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 const app = express();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 app.use(cors());
 
@@ -19,9 +14,8 @@ swaggerSetup(app);
 if (!fs.existsSync(UPLOAD_DIR)) fs.mkdirSync(UPLOAD_DIR, { recursive: true });
 if (!fs.existsSync(OUTPUT_DIR)) fs.mkdirSync(OUTPUT_DIR, { recursive: true });
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(express.json());
+app.use(express.static('public'));
 app.use(router);
 
 app.listen(PORT, () => console.log(`API rodando na porta ${PORT}`));
