dfe12ab8c79c5bc8ecad258a2652667645c578d2
Leonardo Firme <leonardo.ads.ti@gmail.com>
2025-03-15 23:24:17 -0300

    Atualiza server.js e index.html

--- src/server.js
+++ src/server.js
@@ -4,18 +4,27 @@ import { PORT, OUTPUT_DIR, UPLOAD_DIR } from './config.js';
 import router from './routes.js';
 import fs from 'fs';
 import swaggerSetup from './swagger.js';
+import path from 'path';
+import url from 'url'; // Importando o módulo url para manipular o caminho
 
 const app = express();
+
+// Obtendo o __dirname manualmente
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 
 swaggerSetup(app);
 
+// Verificando e criando diretórios, se necessário
 if (!fs.existsSync(UPLOAD_DIR)) fs.mkdirSync(UPLOAD_DIR, { recursive: true });
 if (!fs.existsSync(OUTPUT_DIR)) fs.mkdirSync(OUTPUT_DIR, { recursive: true });
 
+// Servindo arquivos estáticos da pasta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.json());
-app.use(express.static('public'));
 app.use(router);
 
 app.listen(PORT, () => console.log(`API rodando na porta ${PORT}`));
