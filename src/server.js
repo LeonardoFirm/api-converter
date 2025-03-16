33c22808cba53465c8279ee2a85398ad21347f90
Leonardo Firme <102005275+LeonardoFirm@users.noreply.github.com>
2025-03-15 22:34:57 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import { PORT, OUTPUT_DIR, UPLOAD_DIR } from './config.js';
 import router from './routes.js';
 import fs from 'fs';
 import swaggerSetup from './swagger.js';
+import path from 'path'; // Importando o módulo path
 
 const app = express();
 
@@ -11,11 +12,14 @@ app.use(cors());
 
 swaggerSetup(app);
 
+// Verificando e criando diretórios, se necessário
 if (!fs.existsSync(UPLOAD_DIR)) fs.mkdirSync(UPLOAD_DIR, { recursive: true });
 if (!fs.existsSync(OUTPUT_DIR)) fs.mkdirSync(OUTPUT_DIR, { recursive: true });
 
+// Servindo arquivos estáticos da pasta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(express.json());
-app.use(express.static('public'));
 app.use(router);
 
 app.listen(PORT, () => console.log(`API rodando na porta ${PORT}`));
