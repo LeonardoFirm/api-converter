5e31ad3adb7231c0e92edbb0a5a0b73f11bc11b1
Leonardo Firme <leonardo.ads.ti@gmail.com>
2025-03-17 04:24:25 -0300

    Ajustes na aplicação server corrigido

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import { PORT, OUTPUT_DIR, UPLOAD_DIR } from './config.js';
 import router from './routes.js';
 import fs from 'fs';
 import swaggerSetup from './swagger.js';
+import path from 'path';
 
 const app = express();
 
@@ -16,6 +17,11 @@ if (!fs.existsSync(OUTPUT_DIR)) fs.mkdirSync(OUTPUT_DIR, { recursive: true });
 
 app.use(express.json());
 app.use(express.static('public'));
+
+app.get('/favicon.png', (req, res) => {
+  res.sendFile(path.join(process.cwd(), 'public', 'favicon.png'));
+});
+
 app.use(router);
 
 app.listen(PORT, () => console.log(`API rodando na porta ${PORT}`));
