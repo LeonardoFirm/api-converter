ed550f5f9a42a97b1776ccd0bff091424485f351
Leonardo Firme <leonardo.ads.ti@gmail.com>
2025-03-17 04:03:01 -0300

    Ajustes na aplicação server index

--- src/server.js
+++ src/server.js
@@ -4,11 +4,6 @@ import { PORT, OUTPUT_DIR, UPLOAD_DIR } from './config.js';
 import router from './routes.js';
 import fs from 'fs';
 import swaggerSetup from './swagger.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -20,7 +15,7 @@ if (!fs.existsSync(UPLOAD_DIR)) fs.mkdirSync(UPLOAD_DIR, { recursive: true });
 if (!fs.existsSync(OUTPUT_DIR)) fs.mkdirSync(OUTPUT_DIR, { recursive: true });
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.use(router);
 
 app.listen(PORT, () => console.log(`API rodando na porta ${PORT}`));
